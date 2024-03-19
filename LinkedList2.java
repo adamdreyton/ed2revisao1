fbaf04f025d2bc0170190b1b07aae4a625f1e15e
Adam <adamdreyton@unifesspa.edu.br>
2024-03-19 11:41:58 -0300

    addLast tested

--- LinkedList2.java
+++ LinkedList2.java
@@ -1,27 +1,29 @@
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class LinkedList2<Item> implements Iterable<Item>{
+public class LinkedList2<Item> implements Iterable<Item> {
     private Node first;
     private int n;
 
-    private class LinkedIterator implements Iterator<Item>{
+    private class LinkedIterator implements Iterator<Item> {
         private Node current;
 
-        public LinkedIterator(){
+        public LinkedIterator() {
             current = first;
         }
 
-        public boolean hasNext(){
+        @Override
+        public boolean hasNext() {
             return current != null;
         }
 
         @Override
         public Item next() {
-            if(!hasNext()) throw new NoSuchElementException();
+            if (!hasNext())
+                throw new NoSuchElementException();
             Item item = current.item;
             current = current.next;
-            
+
             return item;
         }
     }
@@ -31,19 +33,34 @@ public class LinkedList2<Item> implements Iterable<Item>{
         private Node next;
     }
 
-    public void addFirst(Item item){
+    public void addFirst(Item item) {
         Node newNode = new Node();
         newNode.item = item;
         newNode.next = first;
         first = newNode;
-        n++; 
+        n++;
     }
 
-    public void addLast(Item item){
+    public void addLast(Item item) {
+        if (isEmpty())
+            addFirst(item);
+        else {
+            Node beforeLast = null;
+            Node last = first;
 
+            while (last != null) {
+                beforeLast = last;
+                last = last.next;
+            }
+
+            Node newNode = new Node();
+            newNode.item = item;
+            beforeLast.next = newNode;
+            n++;
+        }
     }
 
-    public int size(){
+    public int size() {
         return n;
     }
 
---
+++ Main3.java
@@ -0,0 +1,22 @@
+
+public class Main3 {
+    public static void main(String[] args) {
+        LinkedList2<Integer> list = new LinkedList2<>();
+
+        list.addFirst(2);
+        list.addFirst(3);
+        list.addFirst(4);
+        list.addFirst(5);
+        list.addLast(10);
+        list.addLast(12);
+        list.addLast(13);
+        list.addFirst(15);
+
+        for (var i : list) {
+            System.out.println(i);
+        }
+
+        System.out.println("Size of list = " + list.size());
+
+    }
+}
